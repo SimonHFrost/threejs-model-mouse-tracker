42d02729902fb89bb7c3cb64284b0521b22c1c60
simonhfrost <simonhfrost@gmail.com>
2018-02-26 15:39:25 +1300

    Remove rotation

--- js/app.js
+++ js/app.js
@@ -12,16 +12,10 @@ var loader = new THREE.JSONLoader()
 var spaceship = null
 var modelPath = 'model/spaceship.json'
 
-function rotateSpaceship () {
-  spaceship.rotation.y -= 0.01
-}
-
 loader.load(modelPath, function (geometry) {
   spaceship = createSpaceship(geometry)
   scene.add(spaceship)
 })
-
-window.setInterval(rotateSpaceship, 20)
 
 scene.add(createAmbientLight())
 scene.add(createDirectionalLight())
