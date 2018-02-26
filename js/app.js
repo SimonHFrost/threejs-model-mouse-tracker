e0f5efa268e225dc4e997b7db633403925cd82e0
simonhfrost <simonhfrost@gmail.com>
2018-02-26 15:45:38 +1300

    Look at mouse while moving

--- js/app.js
+++ js/app.js
@@ -17,5 +17,14 @@ loader.load(modelPath, function (geometry) {
   scene.add(spaceship)
 })
 
+function onDocumentMouseMove( event ) {
+  var mouse = {};
+  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+  spaceship.lookAt(new THREE.Vector3(-mouse.x, 0, mouse.y));
+}
+
+document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
 scene.add(createAmbientLight())
 scene.add(createDirectionalLight())
